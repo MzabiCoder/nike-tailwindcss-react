42f3011b9af39b7a4252193f1455e18df5c6830f
nfannane8287 <nfannane8287@gmail.com>
2025-06-01 17:10:14 -0700

    first commit

---
+++ Desktop/NextJS101/nextapp/app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Team = () => {
+    return (
+        <div>
+            <h1>Team Pages</h1>
+            <p>asjhjdsfh askdjfh adfyueaf sakdjhsd</p>
+        </div>
+    )
+}
+
+export default Team
